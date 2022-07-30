1c8c2fa0102ea19c923dc705f50bdef95bc4dd34
YanwenWang1125 <ywan3625@uwo.ca>
2022-07-30 18:00:39 -0400

    use entity

---
+++ src/main/java/com/blogcloud/entity/User.java
@@ -0,0 +1,16 @@
+package com.blogcloud.entity;
+
+import java.util.Date;
+public class User {
+
+    private Long id;
+    private String nickname;
+    private String username;
+    private String password;
+    private String email;
+    private String avatar;
+    private Integer type;
+    private Date createTime;
+    private Date updateTime;
+
+}
